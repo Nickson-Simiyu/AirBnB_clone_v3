28b9344ac91ce93ce525c69bb96f3cba7671c1e3
Nickson Simiyu <simiyunickson1@gmail.com>
2023-04-29 17:17:48 +0300

    Update test_db_storage.py

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -66,6 +66,29 @@ test_db_storage.py'])
                              "{:s} method needs a docstring".format(func[0]))
             self.assertTrue(len(func[1].__doc__) >= 1,
                             "{:s} method needs a docstring".format(func[0]))
+    def test_get(self):
+        """Test the get method"""
+        state = State(name="California")
+        models.storage.new(state)
+        models.storage.save()
+        state_id = state.id
+        retrieved_state = models.storage.get(State, state_id)
+        self.assertEqual(state, retrieved_state)
+        retrieved_state = models.storage.get(State, "invalid_id")
+        self.assertIsNone(retrieved_state)
+
+    def test_count(self):
+        """Test the count method"""
+        initial_count = models.storage.count()
+        state = State(name="California")
+        models.storage.new(state)
+        models.storage.save()
+        count_with_state = models.storage.count()
+        self.assertEqual(count_with_state, initial_count + 1)
+        count_of_states = models.storage.count(State)
+        self.assertEqual(count_of_states, 1)
+        count_of_nonexistent_class = models.storage.count("InvalidClass")
+        self.assertEqual(count_of_nonexistent_class, 0)
 
 
 class TestFileStorage(unittest.TestCase):
