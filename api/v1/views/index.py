38e34680edb286b4d3bd83b687431857a933323c
Nickson-Simiyu <simiyunickson1@gmail.com>
2023-04-29 11:12:30 -0700

    an endpoint that retrieves the number of each objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 """Index model holds the endpoint (route)"""
-import sys
-sys.path.append('.')
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
