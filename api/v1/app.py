da8619975f6d5122e76282ae874506581ed96a26
Nickson-Simiyu <simiyunickson1@gmail.com>
2023-04-29 07:51:31 -0700

    endpoint (route) will be to return the status of your API

---
+++ api/v1/app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""create the Flask app instance"""
+
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+
+app = Flask(__name__)
+app.register_blueprint(app_views, url_prefix='/api/v1')
+
+@app.teardown_appcontext
+def teardown_app_context(exception):
+    """Teardown application context"""
+    storage.close()
+
+if __name__ == "__main__":
+    host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
+    port = os.environ.get('HBNB_API_PORT', 5000)
+    app.run(host=host, port=port, threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""blueprint that returns a JSON"""
+
+from api.v1.views import app_views
+from flask import jsonify
+
+@app_views.route('/status', methods=['GET'])
+def get_status():
+    return jsonify(status="OK")
