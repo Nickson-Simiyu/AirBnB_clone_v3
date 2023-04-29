5e669f08e581aa66c861656c3c73dd54e96f6f9d
Nickson-Simiyu <simiyunickson1@gmail.com>
2023-04-29 08:55:34 -0700

    file thats starts a flask app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,27 +1,17 @@
 #!/usr/bin/python3
 """endpoint (route) will be to return the status of your API"""
+from flask import Flask
 from api.v1.views import app_views
-from flasgger import Swagger
-from flask import (Blueprint, Flask, jsonify, make_response)
-from flask_cors import (CORS, cross_origin)
 from models import storage
 from os import getenv
 
 
 app = Flask(__name__)
-CORS(app, origins="0.0.0.0")
 app.register_blueprint(app_views)
-Swagger(app)
-
-
-@app.errorhandler(404)
-def not_found(error):
-    """json 404 page"""
-    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 @app.teardown_appcontext
-def teardown(exception):
+def teardown_storage(exception):
     """ closes the session """
     storage.close()
 
@@ -29,5 +19,4 @@ def teardown(exception):
 if __name__ == "__main__":
     host = getenv("HBNB_API_HOST", "0.0.0.0")
     port = getenv("HBNB_API_PORT", "5000")
-#    print(app.url_map)
-    app.run(host=host, port=port)
+    app.run(host=host, port=port, threaded=True)
