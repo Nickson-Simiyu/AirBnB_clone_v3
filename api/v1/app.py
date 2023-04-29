e68d2fe2fe200b3976d128349904ba289ad37f7b
Nickson-Simiyu <simiyunickson1@gmail.com>
2023-04-29 09:31:16 -0700

    a handler for 404 errors that returns a JSON-formatted 404 status code response

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,6 +4,7 @@ from flask import Flask
 from api.v1.views import app_views
 from models import storage
 from os import getenv
+from flask import Flask, jsonify
 
 
 app = Flask(__name__)
@@ -14,6 +15,11 @@ app.register_blueprint(app_views)
 def teardown_storage(exception):
     """ closes the session """
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({'error': 'Not found'}), 404
 
 
 if __name__ == "__main__":
