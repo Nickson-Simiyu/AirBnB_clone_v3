3973af1901918192c2251531d1bfa0567689d71b
Nickson-Simiyu <simiyunickson1@gmail.com>
2023-04-29 09:20:56 -0700

    endpoint that retrieves the number of each objects by type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -13,12 +13,11 @@ def status():
 @app_views.route('/api/v1/stats', methods=['GET'])
 def get_stats():
     """Retrieves the number of each objects by type"""
-    stats = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User")
-    }
+    models_available = {"User": "users",
+                        "Amenity": "amenities", "City": "cities",
+                        "Place": "places", "Review": "reviews",
+                        "State": "states"}
+    stats = {}
+    for cls in models_available.keys():
+        stats[models_available[cls]] = storage.count(cls)
     return jsonify(stats)
