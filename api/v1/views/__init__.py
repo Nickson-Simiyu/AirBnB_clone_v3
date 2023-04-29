0a414c124847f05d5176583db552692cac4ccbbd
Nickson-Simiyu <simiyunickson1@gmail.com>
2023-04-29 11:28:42 -0700

    a new view for City objects that handles all default RESTFul API actions

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -6,3 +6,4 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 # Import everything in the package api.v1.views.index
 from api.v1.views.index import *
 from api.v1.views.states import *
+from api.v1.views.cities import *
