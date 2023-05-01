aa027f2d38f00275d173209e2f0b42af99f24057
Nickson-Simiyu <simiyunickson1@gmail.com>
2023-05-01 14:13:16 -0700

    modified and added two new methods

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -9,8 +9,8 @@ from models.state import State
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_states():
     """Retrieves the list of all State objects"""
-    states = storage.all(State)
-    return jsonify([state.to_dict() for state in states.values()])
+    states = storage.all(State).values()
+    return jsonify([state.to_dict() for state in states])
 
 
 @app_views.route('/states/<string:state_id>', methods=['GET'], strict_slashes=False)
