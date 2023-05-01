46895b8ee4f16fa594761a1e530173d34a570711
Nickson-Simiyu <simiyunickson1@gmail.com>
2023-05-01 15:18:31 -0700

    updated the scripts made the executable

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,22 +1,14 @@
 #!/usr/bin/python3
-"""creates the Blueprint for flask application"""
+""" Blueprint for API """
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-from api.v1.views.places import *
-from api.v1.views.places_reviews import *
-from api.v1.views.places_amenities import *
-from api.v1.views.users import *
 from api.v1.views.index import *
 from api.v1.views.states import *
+from api.v1.views.places import *
+from api.v1.views.places_reviews import *
 from api.v1.views.cities import *
 from api.v1.views.amenities import *
-from os import getenv
-from models.engine.file_storage import FileStorage
-
-
-storage = FileStorage()
-storage.reload()
-
-from models.state import State
+from api.v1.views.users import *
+from api.v1.views.places_amenities import *
