b9a67b3d3079002f26cad117e2d1fa8805e1c2c5
Nickson-Simiyu <simiyunickson1@gmail.com>
2023-05-01 14:47:10 -0700

    updated the scripts made the executable

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -22,7 +22,6 @@ def get_cities(state_id):
 
     return jsonify(list_cities)
 
-
 @app_views.route('/cities/<city_id>/', methods=['GET'], strict_slashes=False)
 def get_city(city_id):
     """
@@ -32,7 +31,6 @@ def get_city(city_id):
     if not city:
         abort(404)
     return jsonify(city.to_dict())
-
 
 @app_views.route('/cities/<city_id>', methods=['DELETE'], strict_slashes=False)
 def delete_city(city_id):
@@ -47,7 +45,6 @@ def delete_city(city_id):
     storage.save()
 
     return make_response(jsonify({}), 200)
-
 
 @app_views.route('/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
 def post_city(state_id):
@@ -67,7 +64,6 @@ def post_city(state_id):
     instance.state_id = state.id
     instance.save()
     return make_response(jsonify(instance.to_dict()), 201)
-
 
 @app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
 def put_city(city_id):
