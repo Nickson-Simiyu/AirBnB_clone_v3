94a61f7a8d920d6c78ac11ccfa6256b267fa1825
Nickson-Simiyu <simiyunickson1@gmail.com>
2023-05-01 15:41:32 -0700

    improved the scripts made them efficient

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Creates a new view for Amenity objects that handles all default RESTFul API actions"""
+"""Amenity view"""
 
 from api.v1.views import app_views
 from flask import abort, jsonify, make_response, request
