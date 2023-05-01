11ac2b9fb226f0e75b8d8dcae33d44f77b72ef29
Nickson-Simiyu <simiyunickson1@gmail.com>
2023-05-01 06:00:58 -0700

    Index model holds the endpoint (route)

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """creates the Blueprint for flask application"""
-from flask import Blueprint
-from api.v1.views.index import *
-from api.v1.views.states import *
-from api.v1.views.cities import *
+from api.v1.views.places_amenities import *
+from api.v1.views.places_reviews import *
+from api.v1.views.places import *
+from api.v1.views.users import *
 from api.v1.views.amenities import *
+from api.v1.views.cities import *
+from api.v1.views.states import *
+from api.v1.views.index import *
+from flask import Blueprint
 
-# Create a Blueprint instance with url prefix '/api/v1'
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
