f0cbc5f60c5fbf056b6173852555cb2503d59699
Nickson-Simiyu <simiyunickson1@gmail.com>
2023-05-02 11:09:24 -0700

    status of api

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,7 +8,7 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 
 
-app = Flask(_name_)
+app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
@@ -24,7 +24,7 @@ def not_found(error):
     return jsonify({'error': 'Not found'}), 404
 
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     host = getenv("HBNB_API_HOST", "0.0.0.0")
     port = getenv("HBNB_API_PORT", "5000")
     app.run(host=host, port=port, threaded=True)
